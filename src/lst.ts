60af141b449ae81a8ebf183075451296b1fb8b12
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-10-25 23:38:25 +0200

    done with eqToAz

--- src/lst.ts
+++ src/lst.ts
@@ -1,22 +1,24 @@
+import { Timestamp, Day, Deg, Rad } from './types';
+import { degToRad, radToDeg, PI2 } from './units';
 import moment = require('moment');
 
-const GST_2017_10_23 = 31.600709; // Reference Greenwich Sidereal Time in degrees
+const GST_2017_10_23 = degToRad(31.600709); // Reference Greenwich Sidereal Time in degrees
 const REFERENCE_TIME = '2017-10-23T00:00:00.000Z';
 const SOLAR_TO_SIDEREAL_FACTOR = 1.002737909350795; // Factor to change sidereal days to solar days
 
-const getElapsedDaysFromReference = (localTime: number): number =>
+const getElapsedDaysFromReference = (localTime: Timestamp): Day =>
   moment(localTime).diff(moment(REFERENCE_TIME), 'days', true);
 
 /**
  * Get Greenwich Local Sidereal Time in degrees
  */
-const getGst = (elapsedDays: number): number => {
-  const sum = GST_2017_10_23 + SOLAR_TO_SIDEREAL_FACTOR * elapsedDays * 360;
-  return sum % 360;
+const getGst = (elapsedDays: Day): Rad => {
+  const sum = GST_2017_10_23 + SOLAR_TO_SIDEREAL_FACTOR * elapsedDays * PI2;
+  return sum % PI2;
 };
 
-export const getLst = (localTime: number, longitude: number): number => {
+export const getLst = (localTime: Timestamp, longitude: Rad): Rad => {
   const elapsedDays = getElapsedDaysFromReference(localTime);
   const gst = getGst(elapsedDays);
-  return (gst + longitude) % 360;
+  return (gst + longitude) % PI2;
 };
--- src/units.ts
+++ src/units.ts
@@ -1,9 +1,24 @@
-import { Hms, Dms } from './types';
+import { Deg, Rad, Hour, Hms, Dms } from './types';
 
-export const degToHours = (deg: number): number => deg / 360 * 24;
+export const PI2 = 2 * Math.PI;
 
-export const degToHms = (deg: number): Hms => {
-  const hourWithDecimals = degToHours(deg < 0 ? 360 + deg : deg);
+export const degToRad = (deg: Deg): Rad => deg / 360 * PI2;
+
+export const radToDeg = (rad: Rad): Deg => rad / PI2 * 360;
+
+export const normalizeRad = (rad: Rad) => {
+  const r = rad % PI2;
+  return r < 0 ? r + PI2 : r;
+};
+
+export const radToHours = (rad: Rad): Hour => normalizeRad(rad) / PI2 * 24;
+
+export const hmsToRad = ({ hour = 0, min = 0, sec = 0 }: Hms): Rad => (hour + (min + sec / 60) / 60) / 24 * PI2;
+
+export const dmsToRad = ({ deg = 0, min = 0, sec = 0 }: Dms): Rad => degToRad(deg + (min + sec / 60) / 60);
+
+export const radToHms = (rad: Rad): Hms => {
+  const hourWithDecimals = radToHours(rad);
   const hour = Math.floor(hourWithDecimals);
   const minWithDecimals = (hourWithDecimals - hour) * 60;
   const min = Math.floor(minWithDecimals);
@@ -12,9 +27,8 @@ export const degToHms = (deg: number): Hms => {
   return { hour, min, sec };
 };
 
-export const hmsToDeg = ({ hour = 0, min = 0, sec = 0 }: Hms): number => (hour + (min + sec / 60) / 60) / 24 * 360;
-
-export const degToDms = (degWithDecimals: number): Dms => {
+export const radToDms = (rad: Rad): Dms => {
+  const degWithDecimals = radToDeg(rad);
   const deg = Math.floor(degWithDecimals);
   const minWithDecimals = (degWithDecimals - deg) * 60;
   const min = Math.floor(minWithDecimals);
@@ -23,4 +37,27 @@ export const degToDms = (degWithDecimals: number): Dms => {
   return { deg, min, sec };
 };
 
-export const dmsToDeg = ({ deg = 0, min = 0, sec = 0 }: Dms): number => deg + (min + sec / 60) / 60;
+/* export const degToHours = (deg: number): number => (deg % 360) / 360 * 24; */
+
+/* export const hmsToDeg = ({ hour = 0, min = 0, sec = 0 }: Hms): number => (hour + (min + sec / 60) / 60) / 24 * 360; */
+
+/* export const dmsToDeg = ({ deg = 0, min = 0, sec = 0 }: Dms): number => deg + (min + sec / 60) / 60; */
+
+/* export const degToHms = (deg: number): Hms => {
+  const hourWithDecimals = degToHours(deg < 0 ? 360 + deg : deg);
+  const hour = Math.floor(hourWithDecimals);
+  const minWithDecimals = (hourWithDecimals - hour) * 60;
+  const min = Math.floor(minWithDecimals);
+  const secWithDecimals = (minWithDecimals - min) * 60;
+  const sec = Math.floor(secWithDecimals);
+  return { hour, min, sec };
+}; */
+
+/* export const degToDms = (degWithDecimals: number): Dms => {
+  const deg = Math.floor(degWithDecimals);
+  const minWithDecimals = (degWithDecimals - deg) * 60;
+  const min = Math.floor(minWithDecimals);
+  const secWithDecimals = (minWithDecimals - min) * 60;
+  const sec = Math.floor(secWithDecimals);
+  return { deg, min, sec };
+}; */
