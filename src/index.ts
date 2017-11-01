6bea8ea0f725ba64d6460df4d412ac8cdfb4d4af
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-01 11:45:31 +0100

    - new radToDms conversion - new hms/dms stringifying

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const catalog = require('../data/ngc.json');
 import { radToHms, radToDmsString, radToHmsString, degToRad, dmsToRad, hmsToRad, hoursToRad } from './units';
 import { eqToAz } from './coords';
 
-const time = '2017-10-30T00:00:00+01:00';
+const time = '2017-10-31T15:00:00+01:00';
 const lat = 47;
 const lon = 20;
 console.log('TIME:', time, 'LAT:', lat, 'LON:', lon);
@@ -14,9 +14,8 @@ const helix = catalog.find(({ ngc }) => ngc === 7293);
 //console.log(andromeda);
 const ra = hmsToRad(helix.eqCoords.ra);
 const de = dmsToRad(helix.eqCoords.de);
-console.log(de)
 const { az, alt } = eqToAz(Date.parse(time), location, { ra, de });
-
+console.log(alt)
 console.log('RA:', radToHmsString(ra), 'DE:', radToDmsString(de));
 console.log('AZ:', radToDmsString(az), 'ALT:', radToDmsString(alt));
 //console.log(azCoords.az, degToDms(azCoords.alt));
