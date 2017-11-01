53c687c9e4c5dc09b50d7650c326a8ba1c159055
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-01 17:29:15 +0100

    remove comments

--- src/lst.ts
+++ src/lst.ts
@@ -2,9 +2,7 @@ import { Timestamp, Day, Deg, Rad } from './types';
 import { degToRad, radToDeg, PI2, hmsToRad } from './units';
 import moment = require('moment');
 
-//const GST_REFERENCE = hmsToRad({ hour: 6, min: 43, sec: 21.2 });
 const GST_REFERENCE = hmsToRad({ hour: 2, min: 37, sec: 57.4 });
-//const GST_REFERENCE_TIME = Date.parse('2017-01-01T00:00:00.000Z');
 const GST_REFERENCE_TIME = Date.parse('2017-10-31T00:00:00.000Z');
 const EARTH_ANGLULAR_SPEED = 0.00007292115146706924; // radians per second
 
