bac6838926d47c55725e8b641ea9cc7938428572
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-01 21:35:51 +0100

    halfDayArcs enhancements

--- src/halfDaysArcs.ts
+++ src/halfDaysArcs.ts
@@ -1,17 +1,31 @@
-import { Timestamp, Loc, Eq, HalfDayArc } from './types';
+import { Timestamp, Loc, Eq, HalfDayArc, Rad } from './types';
 import { hmsToRad, dmsToRad, PI2 } from './units';
 import { timeToLst, lstToTime } from './lst';
 import { getEqCoordsOnDate } from './corrections';
 
-const { asin, acos, tan, ceil, PI, sin, cos, floor } = Math;
+const { asin, acos, tan, ceil, PI, sin, cos, floor, sqrt } = Math;
+
+export const isRising = (siderealTime: Rad, lat: Rad, ra: Rad, de: Rad): boolean => {
+  const t = siderealTime;
+  const cosDe = cos(de);
+  const cosLat = cos(lat);
+  const ha = ra - t;
+  const derivate = cosDe * cosLat * sin(ha) / sqrt(1 - (cosDe * cosLat * cos(ha) + sin(de) * sin(lat)));
+  return derivate > 0;
+};
 
 export const getHalfDayArcFactory = (noon: Timestamp, { lat, lon }: Loc) => (eqCoordsOnJ2000: Eq): HalfDayArc => {
   const siderealNoon = timeToLst(noon, lon, false);
   const { ra, de } = getEqCoordsOnDate(eqCoordsOnJ2000, noon);
   const psi = acos(-tan(lat) * tan(de));
-  const nextCrossing1 = ra + psi;
-  const nextCrossing2 = ra - psi;
-  console.log(new Date(lstToTime(nextCrossing1, lon)).toLocaleString());
-  console.log(new Date(lstToTime(nextCrossing2, lon)).toLocaleString());
-  return { rise: 0, set: 0 };
+  if (!Number.isFinite(psi)) return {};
+  const k1 = floor((ra + psi - siderealNoon) / PI2);
+  const k2 = floor((ra - psi - siderealNoon) / PI2);
+  const nextCrossing1 = ra + psi - PI2 * k1;
+  const nextCrossing2 = ra - psi - PI2 * k2;
+  const startsWithRising = isRising(nextCrossing1, lat, ra, de);
+  return {
+    rise: lstToTime(startsWithRising ? nextCrossing1 : nextCrossing2, lon),
+    set: lstToTime(startsWithRising ? nextCrossing2 : nextCrossing1, lon)
+  };
 };
