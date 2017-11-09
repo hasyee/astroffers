10b293f3eaa97a69678f5dbd13a864e2fc26f598
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-09 18:53:05 +0100

    wip

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import AppBar from 'material-ui/AppBar';
 import DatePicker from 'material-ui/DatePicker';
-import Panel from './Panel';
+import AppBar from 'material-ui/AppBar';
+import Filter from './Filter';
 
 export default class extends React.PureComponent {
   render() {
     return (
-      <div className="absolute layout">
-        <Panel />
-        <div className="fitted layout" />
+      <div className="absolute column layout">
+        <header className="dynamic">
+          <AppBar title="Astroffers" showMenuIconButton={false} />
+        </header>
+        <main className="fitted layout">
+          <Filter />
+        </main>
       </div>
     );
   }
