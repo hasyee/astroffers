48e8da9a293ba8e8ad6e3ad00229f0f437928f36
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-09 16:24:26 +0100

    wip

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
+import AppBar from 'material-ui/AppBar';
 
 export default class extends React.PureComponent {
   render() {
-    return <div>hello</div>;
+    return (
+      <div className="absolute layout">
+        <div className="dynamic column layout panel">
+          <AppBar title="Astroffers" showMenuIconButton={false} />
+        </div>
+        <div className="fitted layout" />
+      </div>
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import store from './store';
 import App from './components/App';
 
+const theme = {
+  palette: {
+    primary1Color: '#ffeb3b',
+    accent1Color: '#ff5252',
+    accent3Color: '#ff8a80',
+    accent2Color: '#ff1744'
+  }
+};
+
 ReactDOM.render(
   <Provider store={store as any}>
-    <App />
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme, theme)}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('app')
 );
