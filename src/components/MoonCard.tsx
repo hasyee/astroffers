8f1db8d102d39b49d963e694663f542781ac24bf
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-11 19:56:23 +0100

    wip

--- src/components/MoonCard.tsx
+++ src/components/MoonCard.tsx
@@ -2,7 +2,8 @@ import React = require('react');
 import moment = require('moment');
 import { connect } from 'react-redux';
 import FontIcon from 'material-ui/FontIcon';
-import { getHalfDayArcOfMoon } from '../calcs/moon';
+import { getNight } from '../calcs/sun';
+import { getMoonNight } from '../calcs/moon';
 import { getLocation } from '../calcs/units';
 
 const iconStyle = {
@@ -13,18 +14,19 @@ const iconStyle = {
 class MoonCard extends React.PureComponent<{ date: number; latitude: number; longitude: number }> {
   render() {
     const { date, latitude, longitude } = this.props;
-    const { start, end } = getHalfDayArcOfMoon(date, getLocation(latitude, longitude));
+    const night = getNight(date, getLocation(latitude, longitude));
+    const { start, end } = getMoonNight(night, getLocation(latitude, longitude));
     return (
       <div className="dynamic row layout half-day-arc card center">
         <div className="center layout">
           <FontIcon className="mdi mdi-brightness-2" style={iconStyle} />
         </div>
         <div className="fitted column layout">
-          <div className="center layout">Moonrise</div>
+          <div className="center layout">Moonset</div>
           <div className="center layout">{moment(start).format('HH:mm')}</div>
         </div>
         <div className="fitted column layout">
-          <div className="center layout">Moonset</div>
+          <div className="center layout">Moonrise</div>
           <div className="center layout">{moment(end).format('HH:mm')}</div>
         </div>
       </div>
