c0d31b93bdabd0b70cf12b6a58b1c2a6ced19b15
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-14 00:45:04 +0100

    resolve https://github.com/jayhasyee/astroffers/issues/3

--- src/store.ts
+++ src/store.ts
@@ -1,18 +1,19 @@
 import Store, { thunk } from 'repatch';
 import { State } from './types';
+import { fetchLocation } from './api';
 
 const initialState: State = {
   filter: {
     date: Date.now(),
     magnitude: 10,
     latitude: 47,
-    longitude: 20,
+    longitude: 19,
     twilight: -18,
     altitude: 20
   }
 };
 
-const store = new Store<State>(initialState).addMiddleware(thunk.withExtraArgument({}));
+const store = new Store<State>(initialState).addMiddleware(thunk.withExtraArgument({ fetchLocation }));
 
 window['store'] = store;
 
