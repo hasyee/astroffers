ca5c29f7137c311b5eb500d1d50af26d3f305026
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-13 23:54:17 +0100

    fix https://github.com/jayhasyee/astroffers/issues/15

--- src/calcs/getObjects.ts
+++ src/calcs/getObjects.ts
@@ -1,7 +1,7 @@
 import { NgcObject, NgcInfo, NightInfo, Loc, Timestamp, Deg, Interval } from './types';
 import { getHalfDayArcFactory } from './halfDaysArcs';
 import { getLocation, degToRad, hmsToRad, dmsToRad } from './units';
-import { toNoon } from './time';
+import { toNoon, toMidnight } from './time';
 import { getIntersection, isInInterval } from './interval';
 import { getEqCoordsOnDate } from './corrections';
 import { eqToAz } from './coords';
@@ -24,23 +24,20 @@ export default (
   magnitudeLimit: number
 ): NgcInfo[] => {
   const location = getLocation(latitude, longitude);
-  const ngcNoon = toNoon(date);
-  const getHalfDayArc = getHalfDayArcFactory(ngcNoon, location, degToRad(altitideLimit));
+  const refTime = astroNight.start;
+  const getHalfDayArc = getHalfDayArcFactory(refTime, location, degToRad(altitideLimit));
   return ngcObjects
     .filter(object => Number.isFinite(object.magnitude) && object.magnitude < magnitudeLimit)
     .map(object => {
       const ra = hmsToRad(object.eqCoords.ra);
       const de = dmsToRad(object.eqCoords.de);
       const eqCoordsOnJ2000 = { ra, de };
-      const eqCoordsOnDate = getEqCoordsOnDate(eqCoordsOnJ2000, ngcNoon);
+      const eqCoordsOnDate = getEqCoordsOnDate(eqCoordsOnJ2000, refTime);
       const hda = getHalfDayArc(eqCoordsOnDate);
       const transit = Math.round((hda.start + hda.end) / 2);
       const intersection = getIntersection(hda, astroNight);
       const max = getMax(intersection, transit);
       const { alt: altitudeAtMax } = eqToAz(max, location, eqCoordsOnDate);
-      /* if (object.ngc === 7331) {
-        console.log(hda, intersection);
-      } */
       return { object, eqCoordsOnDate, intersection, transit, max, altitudeAtMax };
     })
     .filter(ngcInfo => ngcInfo.intersection);
