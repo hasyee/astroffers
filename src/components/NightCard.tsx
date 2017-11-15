4255bd42dbf669f539e4738cb9e55c16bf9eae91
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-15 02:10:15 +0100

    styling night card

--- src/components/NightCard.tsx
+++ src/components/NightCard.tsx
@@ -8,39 +8,44 @@ class NightCard extends React.PureComponent<{ nightInfo: NightInfo; isFiltering:
     if (!this.props.nightInfo || this.props.isFiltering) return null;
     const { night, astroNight, moonNight, moonlessNight } = this.props.nightInfo;
     return (
-      <div className="dynamic night card center">
-        <table>
-          <tbody>
-            <tr>
-              <td>Night</td>
-              <td>Sunset</td>
-              <td>{moment(night.start).format('HH:mm')}</td>
-              <td>Sunrise</td>
-              <td>{moment(night.end).format('HH:mm')}</td>
-            </tr>
-            <tr>
-              <td>Astro night</td>
-              <td>From</td>
-              <td>{moment(astroNight.start).format('HH:mm')}</td>
-              <td>To</td>
-              <td>{moment(astroNight.end).format('HH:mm')}</td>
-            </tr>
-            <tr>
-              <td>Moon</td>
-              <td>Moonset</td>
-              <td>{moment(moonNight.start).format('HH:mm')}</td>
-              <td>Moonrise</td>
-              <td>{moment(moonNight.end).format('HH:mm')}</td>
-            </tr>
-            <tr>
-              <td>Moonless night</td>
-              <td>From</td>
-              <td>{moment(moonlessNight.start).format('HH:mm')}</td>
-              <td>To</td>
-              <td>{moment(moonlessNight.end).format('HH:mm')}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="dynamic row layout night card">
+        <div className="dynamic layout center">
+          <i className="mdi mdi-weather-night" />
+        </div>
+        <div className="fitted layout center">
+          <table>
+            <tbody>
+              <tr>
+                <td>Night</td>
+                <td>Sunset</td>
+                <td>{moment(night.start).format('HH:mm')}</td>
+                <td>Sunrise</td>
+                <td>{moment(night.end).format('HH:mm')}</td>
+              </tr>
+              <tr>
+                <td>Astro night</td>
+                <td>From</td>
+                <td>{moment(astroNight.start).format('HH:mm')}</td>
+                <td>To</td>
+                <td>{moment(astroNight.end).format('HH:mm')}</td>
+              </tr>
+              <tr>
+                <td>Moon</td>
+                <td>Moonset</td>
+                <td>{moment(moonNight.start).format('HH:mm')}</td>
+                <td>Moonrise</td>
+                <td>{moment(moonNight.end).format('HH:mm')}</td>
+              </tr>
+              <tr>
+                <td>Moonless night</td>
+                <td>From</td>
+                <td>{moment(moonlessNight.start).format('HH:mm')}</td>
+                <td>To</td>
+                <td>{moment(moonlessNight.end).format('HH:mm')}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
