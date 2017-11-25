a643208423a39cc7965cd4461af9dfebcbe25889
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-25 21:51:22 +0100

    wip auto-updater

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,38 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
+import { autoUpdater } from 'electron-updater';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+
+/* function sendStatusToWindow(text) {
+  console.log(text)
+  win.webContents.send('message', text);
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', info => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', info => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', err => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', progressObj => {
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', info => {
+  sendStatusToWindow('Update downloaded');
+}); */
 
 function createWindow() {
   // Create the browser window.
@@ -36,6 +64,8 @@ function createWindow() {
     win = null;
     app.quit();
   });
+
+  autoUpdater.checkForUpdates().then((...args) => console.log(...args));
 }
 
 // This method will be called when Electron has finished
