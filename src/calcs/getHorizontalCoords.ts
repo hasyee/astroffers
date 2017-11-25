db21810e0757b369cc821a72e82a3f79ebedbd4f
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-25 17:39:14 +0100

    resolve https://github.com/jayhasyee/astroffers/issues/10

---
+++ src/calcs/getHorizontalCoords.ts
@@ -0,0 +1,18 @@
+import { Timestamp, Deg, Eq, Az, CoordSeries } from './types';
+import { getLocation } from './units';
+import { toNoon, toNextDay } from './time';
+import { eqToAz } from './coords';
+
+export default (time: Timestamp, latitude: Deg, longitude: Deg, eq: Eq): CoordSeries<Az> => {
+  const location = getLocation(latitude, longitude);
+  const todayNoon = toNoon(time);
+  const nextDayNoon = toNextDay(todayNoon);
+  const length = (nextDayNoon - todayNoon) / 1000 / 60;
+  return Array.from({ length }).map((_, i) => {
+    const time = todayNoon + i * 1000 * 60;
+    return {
+      time,
+      coord: eqToAz(time, location, eq)
+    };
+  });
+};
---
+++ src/components/AzimuthChart.tsx
@@ -0,0 +1,170 @@
+import React = require('react');
+import ReactHighcharts = require('react-highcharts');
+import HighchartsMore = require('highcharts-more');
+import moment = require('moment');
+import { NgcInfo, NightInfo, Interval, Az, CoordSeries } from '../calcs/types';
+import { radToDeg } from '../calcs/units';
+import { toNextDay } from '../calcs/time';
+import { getCiphers } from 'crypto';
+
+HighchartsMore(ReactHighcharts.Highcharts);
+
+export default class extends React.PureComponent<{
+  minAltitude: number;
+  ngcInfo: NgcInfo;
+  nightInfo: NightInfo;
+  horizontalCoords: CoordSeries<Az>;
+}> {
+  render() {
+    return (
+      <ReactHighcharts
+        config={getConfig(
+          this.props.horizontalCoords,
+          this.props.ngcInfo,
+          this.props.nightInfo,
+          this.props.minAltitude
+        )}
+      />
+    );
+  }
+}
+
+const getNightBands = (interval: Interval, color: string) => {
+  if (!interval) return [];
+  const { start, end } = interval;
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  const overhanging = startDate.getDay() !== endDate.getDay();
+  const startHours = startDate.getHours() + startDate.getMinutes() / 60;
+  const endHours = endDate.getHours() + endDate.getMinutes() / 60;
+  const baseBands = overhanging
+    ? [ { from: startHours, to: 24 }, { from: 0, to: endHours } ]
+    : [ { from: startHours, to: endHours } ];
+  return baseBands.map(b => ({ ...b, thickness: 50, color }));
+};
+
+const getConfig = (
+  horizontalCoords: CoordSeries<Az>,
+  { transit, max }: NgcInfo,
+  { night, moonlessNight, astroNight }: NightInfo,
+  minAltitude: number
+) => {
+  const data = horizontalCoords.map(({ time, coord: { alt } }) => ({ x: time, y: radToDeg(alt) }));
+  return {
+    plotOptions: {
+      series: {
+        turboThreshold: 1500
+      }
+    },
+
+    chart: {
+      height: '200px'
+    },
+
+    title: {
+      text: ''
+    },
+
+    legend: {
+      enabled: false
+    },
+
+    exporting: {
+      enabled: false
+    },
+
+    tooltip: {
+      borderWidth: 0,
+      formatter: function() {
+        return `${moment(this.x).format('HH:mm')} - ${Math.round(this.y)}°`;
+      }
+    },
+
+    xAxis: {
+      tickInterval: 1000 * 3600 * 2,
+
+      labels: {
+        formatter: function() {
+          return moment(this.value).format('HH:mm');
+        }
+      },
+
+      plotBands: [
+        {
+          from: data[0].x,
+          to: data[data.length - 1].x,
+          color: 'lightblue'
+        },
+        {
+          from: night.start,
+          to: night.end,
+          color: '#5c6bc0'
+        },
+        {
+          from: astroNight.start,
+          to: astroNight.end,
+          color: 'grey'
+        },
+        ,
+        {
+          from: moonlessNight.start,
+          to: moonlessNight.end,
+          color: 'black'
+        }
+      ],
+
+      plotLines: [
+        {
+          value: transit < data[0].x ? toNextDay(transit) : transit,
+          zIndex: 5,
+          width: 1,
+          color: 'white',
+          label: {
+            text: 'Transit',
+            style: {
+              color: 'white'
+            }
+          }
+        },
+        {
+          value: max,
+          zIndex: 5,
+          width: 1,
+          color: 'white',
+          label: {
+            text: 'Best visibility',
+            style: {
+              color: 'white',
+              fontWeight: 'bold'
+            }
+          }
+        }
+      ]
+    },
+
+    yAxis: {
+      title: {
+        text: 'Altitude °'
+      },
+
+      plotLines: [
+        {
+          value: minAltitude,
+          zIndex: 5,
+          width: 2,
+          dashStyle: 'dash',
+          color: 'white'
+        }
+      ]
+    },
+
+    series: [
+      {
+        name: 'Altitude',
+        color: '#2196F3',
+        lineWidth: 3,
+        data
+      }
+    ]
+  };
+};
