f6ff994361d6df81346e3e35262ce12e89a95d31
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-26 23:28:27 +0100

    fixes

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -3,13 +3,14 @@ import ReactMarkdown = require('react-markdown');
 import { readFileSync } from 'fs';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import { join } from 'path';
 
 export default class extends React.PureComponent<{
   isOpen: boolean;
   onClose: Function;
 }> {
   render() {
-    const md = readFileSync('static/help.md').toString();
+    const md = readFileSync(join(__dirname, '../../static/help.md')).toString();
     const { isOpen, onClose } = this.props;
     const actions = [ <FlatButton label="Close" primary={true} onClick={onClose} /> ];
     return (
