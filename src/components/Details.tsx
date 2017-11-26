a4913f555e170da07c13d4d00e6bf32afbe9ce9e
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-26 10:44:12 +0100

    styling

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -63,116 +63,123 @@ class Details extends React.PureComponent<{
           <div className="dynamic row layout">
             <div className="fitted layout">
               <table>
-                <tr>
-                  <td>
-                    <b>Type</b>
-                  </td>
-                  <td>{resolveTypes(type).join(', ')}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Constellation</b>
-                  </td>
-                  <td>{constellation}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Size</b>
-                  </td>
-                  <td>{size ? `${dmsToString(size[0])} × ${dmsToString(size[1])}` : 'Unknown'}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Magnitude</b>
-                  </td>
-                  <td>{magnitude}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Surface brightness</b>
-                  </td>
-                  <td>{surfaceBrightness}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>RA (J2000)</b>
-                  </td>
-                  <td>{hmsToString(eqCoords.ra)}</td>
-                  <td>
-                    <b>Dec (J2000)</b>
-                  </td>
-                  <td>{dmsToString(eqCoords.de)}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>RA (on date)</b>
-                  </td>
-                  <td>{radToHmsString(eqCoordsOnDate.ra)}</td>
-                  <td>
-                    <b>Dec (on date)</b>
-                  </td>
-                  <td>{radToDmsString(eqCoordsOnDate.de)}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Rising</b>
-                  </td>
-                  <td>{moment(hda0.start).format('HH:mm')}</td>
-                  <td>
-                    <b>Setting</b>
-                  </td>
-                  <td>{moment(hda0.end).format('HH:mm')}</td>
-                </tr>
-                {minAltitude !== 0 && (
+                <tbody>
                   <tr>
                     <td>
-                      <b>Rising above {minAltitude}°</b>
+                      <b>Type</b>
                     </td>
-                    <td>{moment(hda.start).format('HH:mm')}</td>
-                    <td>
-                      <b>Setting below {minAltitude}°</b>
-                    </td>
-                    <td>{moment(hda.end).format('HH:mm')}</td>
+                    <td>{resolveTypes(type).join(', ')}</td>
                   </tr>
-                )}
-                <tr>
-                  <td>
-                    <b>Visibility from</b>
-                  </td>
-                  <td>{moment(intersection.start).format('HH:mm')}</td>
-                  <td>
-                    <b>Visibility to</b>
-                  </td>
-                  <td>{moment(intersection.end).format('HH:mm')}</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Best visibility</b>
-                  </td>
-                  <td>{moment(max).format('HH:mm')}</td>
-                  <td>
-                    <b>Altitude</b>
-                  </td>
-                  <td>{Math.round(radToDeg(altitudeAtMax))}°</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Transit</b>
-                  </td>
-                  <td>{moment(transit).format('HH:mm')}</td>
-                  <td>
-                    <b>Altitude</b>
-                  </td>
-                  <td>{Math.round(radToDeg(altitudeAtTransit))}°</td>
-                </tr>
+                  <tr>
+                    <td>
+                      <b>Constellation</b>
+                    </td>
+                    <td>{constellation}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Size</b>
+                    </td>
+                    <td>{size ? `${dmsToString(size[0])} × ${dmsToString(size[1])}` : 'Unknown'}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Magnitude</b>
+                    </td>
+                    <td>{magnitude}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Surface brightness</b>
+                    </td>
+                    <td>{surfaceBrightness}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>RA (J2000)</b>
+                    </td>
+                    <td>{hmsToString(eqCoords.ra)}</td>
+                    <td>
+                      <b>Dec (J2000)</b>
+                    </td>
+                    <td>{dmsToString(eqCoords.de)}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>RA (on date)</b>
+                    </td>
+                    <td>{radToHmsString(eqCoordsOnDate.ra)}</td>
+                    <td>
+                      <b>Dec (on date)</b>
+                    </td>
+                    <td>{radToDmsString(eqCoordsOnDate.de)}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Rising</b>
+                    </td>
+                    <td>{moment(hda0.start).format('HH:mm')}</td>
+                    <td>
+                      <b>Setting</b>
+                    </td>
+                    <td>{moment(hda0.end).format('HH:mm')}</td>
+                  </tr>
+                  {minAltitude !== 0 && (
+                    <tr>
+                      <td>
+                        <b>Rising above {minAltitude}°</b>
+                      </td>
+                      <td>{moment(hda.start).format('HH:mm')}</td>
+                      <td>
+                        <b>Setting below {minAltitude}°</b>
+                      </td>
+                      <td>{moment(hda.end).format('HH:mm')}</td>
+                    </tr>
+                  )}
+                  <tr>
+                    <td>
+                      <b>Visibility from</b>
+                    </td>
+                    <td>{moment(intersection.start).format('HH:mm')}</td>
+                    <td>
+                      <b>Visibility to</b>
+                    </td>
+                    <td>{moment(intersection.end).format('HH:mm')}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Best visibility</b>
+                    </td>
+                    <td>{moment(max).format('HH:mm')}</td>
+                    <td>
+                      <b>Altitude</b>
+                    </td>
+                    <td>{Math.round(radToDeg(altitudeAtMax))}°</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>Transit</b>
+                    </td>
+                    <td>{moment(transit).format('HH:mm')}</td>
+                    <td>
+                      <b>Altitude</b>
+                    </td>
+                    <td>{Math.round(radToDeg(altitudeAtTransit))}°</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
             <div className="dynamic layout">
-              <img alt={`preview of ${ngc}`} src={getImgSrc(ngc)} width="300px" height="300px" />
+              <img alt={`preview of ${ngc}`} src={getImgSrc(ngc)} width="250px" height="250px" />
             </div>
           </div>
           <div>
-            <AzimuthChart minAltitude={minAltitude} ngcInfo={this.props.ngcInfo} horizontalCoords={horizontalCoords} nightInfo={nightInfo} />
+            <AzimuthChart
+              minAltitude={minAltitude}
+              ngcInfo={this.props.ngcInfo}
+              horizontalCoords={horizontalCoords}
+              nightInfo={nightInfo}
+            />
           </div>
         </div>
       </Dialog>
