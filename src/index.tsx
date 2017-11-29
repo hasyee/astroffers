3e6ddb4ca48cceda0088f86f042c95ce8497d0bc
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-11-29 19:16:04 +0100

    turn off dev metrics

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import ReactHighcharts = require('react-highcharts');
 import HighchartsMore = require('highcharts/highcharts-more');
 import Nucleus = require('electron-nucleus');
 
-Nucleus.init('5a1ee99ffd2a27796bfc934f', true);
+Nucleus.init('5a1ee99ffd2a27796bfc934f');
 
 try {
   HighchartsMore(ReactHighcharts.Highcharts);
