0cc8e36e73b34f5b123adf6a43b7bc3e4421b940
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-12-03 17:45:29 +0100

    refact react-redux connecting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,82 +12,82 @@ import Help from './Help';
 import Result from './Result';
 import { track, trackScreen } from '../actions';
 
-class App extends React.PureComponent<{
-  track: typeof track;
-  trackScreen: typeof trackScreen;
-}> {
-  state = {
-    isAboutOpen: false,
-    isHelpOpen: false
-  };
+export default connect(null, { track, trackScreen })(
+  class extends React.PureComponent<{
+    track: typeof track;
+    trackScreen: typeof trackScreen;
+  }> {
+    state = {
+      isAboutOpen: false,
+      isHelpOpen: false
+    };
 
-  handleAboutOpen = () => {
-    this.setState({ isAboutOpen: true });
-    this.props.track('About', 'open');
-  };
-  handleAboutClose = () => {
-    this.setState({ isAboutOpen: false });
-    this.props.track('About', 'close');
-  };
-  handleHelpOpen = () => {
-    this.setState({ isHelpOpen: true });
-    this.props.track('Help', 'open');
-  };
-  handleHelpClose = () => {
-    this.setState({ isHelpOpen: false });
-    this.props.track('Help', 'close');
-  };
+    handleAboutOpen = () => {
+      this.setState({ isAboutOpen: true });
+      this.props.track('About', 'open');
+    };
+    handleAboutClose = () => {
+      this.setState({ isAboutOpen: false });
+      this.props.track('About', 'close');
+    };
+    handleHelpOpen = () => {
+      this.setState({ isHelpOpen: true });
+      this.props.track('Help', 'open');
+    };
+    handleHelpClose = () => {
+      this.setState({ isHelpOpen: false });
+      this.props.track('Help', 'close');
+    };
 
-  componentDidMount() {
-    this.props.trackScreen('App');
+    componentDidMount() {
+      this.props.trackScreen('App');
+    }
+
+    renderMenu() {
+      return (
+        <IconMenu
+          iconButtonElement={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem primaryText="Help" onClick={this.handleHelpOpen} />
+          <MenuItem primaryText="About" onClick={this.handleAboutOpen} />
+        </IconMenu>
+      );
+    }
+
+    render() {
+      return (
+        <div className="absolute column layout">
+          <header className="dynamic">
+            <AppBar
+              title={
+                <div>
+                  <img src="static/icons/icon_transparent_white.png" />
+                  <span>Astroffers</span>
+                </div>
+              }
+              showMenuIconButton={false}
+              iconElementRight={this.renderMenu()}
+              titleStyle={{ fontWeight: '100', userSelect: 'none', cursor: 'default' }}
+            />
+          </header>
+          <main className="fitted layout">
+            <div className="dynamic column layout high panel">
+              <Filter />
+            </div>
+            <div className="fitted column layout overflow-x">
+              <Result />
+            </div>
+          </main>
+          <About isOpen={this.state.isAboutOpen} onClose={this.handleAboutClose} />
+          <Help isOpen={this.state.isHelpOpen} onClose={this.handleHelpClose} />
+        </div>
+      );
+    }
   }
-
-  renderMenu() {
-    return (
-      <IconMenu
-        iconButtonElement={
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
-        }
-        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-      >
-        <MenuItem primaryText="Help" onClick={this.handleHelpOpen} />
-        <MenuItem primaryText="About" onClick={this.handleAboutOpen} />
-      </IconMenu>
-    );
-  }
-
-  render() {
-    return (
-      <div className="absolute column layout">
-        <header className="dynamic">
-          <AppBar
-            title={
-              <div>
-                <img src="static/icons/icon_transparent_white.png" />
-                <span>Astroffers</span>
-              </div>
-            }
-            showMenuIconButton={false}
-            iconElementRight={this.renderMenu()}
-            titleStyle={{ fontWeight: '100', userSelect: 'none', cursor: 'default' }}
-          />
-        </header>
-        <main className="fitted layout">
-          <div className="dynamic column layout high panel">
-            <Filter />
-          </div>
-          <div className="fitted column layout overflow-x">
-            <Result />
-          </div>
-        </main>
-        <About isOpen={this.state.isAboutOpen} onClose={this.handleAboutClose} />
-        <Help isOpen={this.state.isHelpOpen} onClose={this.handleHelpClose} />
-      </div>
-    );
-  }
-}
-
-export default connect(null, { track, trackScreen })(App);
+);
--- src/components/SelectLocationDialog.tsx
+++ src/components/SelectLocationDialog.tsx
@@ -6,75 +6,75 @@ import Map from './Map';
 import { fetchLocation } from '../api';
 import { track } from '../actions';
 
-class SelectLocationDialog extends React.PureComponent<
-  {
-    isOpen: boolean;
-    latitude: number;
-    longitude: number;
-    onSubmit: Function;
-    onCancel: Function;
-    track: typeof track;
-  },
-  { latitude: number; longitude: number }
-> {
-  constructor(props) {
-    super(props);
-    this.state = this.getInitialState();
-  }
+export default connect(null, { track })(
+  class extends React.PureComponent<
+    {
+      isOpen: boolean;
+      latitude: number;
+      longitude: number;
+      onSubmit: Function;
+      onCancel: Function;
+      track: typeof track;
+    },
+    { latitude: number; longitude: number }
+  > {
+    constructor(props) {
+      super(props);
+      this.state = this.getInitialState();
+    }
 
-  getInitialState() {
-    return {
-      latitude: this.props.latitude,
-      longitude: this.props.longitude
+    getInitialState() {
+      return {
+        latitude: this.props.latitude,
+        longitude: this.props.longitude
+      };
+    }
+
+    handleFetchLocation = async () => {
+      const { latitude, longitude } = await fetchLocation();
+      if (Number.isFinite(latitude) && Number.isFinite(longitude)) this.setState({ latitude, longitude });
+      this.props.track('Network Location', 'fetch', { evLabel: 'coord', evValue: latitude + '_' + longitude });
     };
-  }
 
-  handleFetchLocation = async () => {
-    const { latitude, longitude } = await fetchLocation();
-    if (Number.isFinite(latitude) && Number.isFinite(longitude)) this.setState({ latitude, longitude });
-    this.props.track('Network Location', 'fetch', { evLabel: 'coord', evValue: latitude + '_' + longitude });
-  };
+    handleChange = state => this.setState(state);
 
-  handleChange = state => this.setState(state);
+    handleSubmit = () => {
+      this.props.onSubmit(this.state);
+    };
 
-  handleSubmit = () => {
-    this.props.onSubmit(this.state);
-  };
+    handleCancel = () => {
+      this.setState(this.getInitialState());
+      this.props.onCancel();
+    };
 
-  handleCancel = () => {
-    this.setState(this.getInitialState());
-    this.props.onCancel();
-  };
+    componentWillUpdate(nextProps) {
+      if (nextProps.latitude !== this.props.latitude || nextProps.longitude !== this.props.longitude) {
+        this.setState({
+          latitude: nextProps.latitude,
+          longitude: nextProps.longitude
+        });
+      }
+    }
 
-  componentWillUpdate(nextProps) {
-    if (nextProps.latitude !== this.props.latitude || nextProps.longitude !== this.props.longitude) {
-      this.setState({
-        latitude: nextProps.latitude,
-        longitude: nextProps.longitude
-      });
+    render() {
+      const { onSubmit, isOpen } = this.props;
+      const { latitude, longitude } = this.state;
+      const actions = [
+        <FlatButton label="Use network location" onClick={this.handleFetchLocation} style={{ float: 'left' }} />,
+        <FlatButton label="Cancel" onClick={this.handleCancel} />,
+        <FlatButton label="Submit" primary={true} onClick={this.handleSubmit} />
+      ];
+      return (
+        <Dialog title="Select your location" actions={actions} modal open={isOpen} onRequestClose={this.handleCancel}>
+          <Map
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            latitude={latitude}
+            longitude={longitude}
+            onChange={this.handleChange}
+          />
+        </Dialog>
+      );
     }
   }
-
-  render() {
-    const { onSubmit, isOpen } = this.props;
-    const { latitude, longitude } = this.state;
-    const actions = [
-      <FlatButton label="Use network location" onClick={this.handleFetchLocation} style={{ float: 'left' }} />,
-      <FlatButton label="Cancel" onClick={this.handleCancel} />,
-      <FlatButton label="Submit" primary={true} onClick={this.handleSubmit} />
-    ];
-    return (
-      <Dialog title="Select your location" actions={actions} modal open={isOpen} onRequestClose={this.handleCancel}>
-        <Map
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-          latitude={latitude}
-          longitude={longitude}
-          onChange={this.handleChange}
-        />
-      </Dialog>
-    );
-  }
-}
-
-export default connect(null, { track })(SelectLocationDialog);
+);
