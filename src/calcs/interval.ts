4d87eaf3eb6805f4c3480e95eb034d2129be08d9
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-12-05 00:06:06 +0100

    wip fixing circumpolar objects

--- src/calcs/interval.ts
+++ src/calcs/interval.ts
@@ -1,28 +1,18 @@
 import { Interval, Timestamp } from './types';
 
-const prepareInterval = ({ start, end }: Interval): Interval => ({
-  start: Number.isFinite(start) ? start : Infinity,
-  end: Number.isFinite(end) ? end : Infinity
-});
-
-const unprepareInterval = ({ start, end }: Interval): Interval => ({
-  start: Number.isFinite(start) ? start : undefined,
-  end: Number.isFinite(end) ? end : undefined
-});
-
 export const getIntersection = (interval1: Interval, interval2: Interval): Interval => {
   if (!interval1 || !interval2) return null;
-  const { start: start1, end: end1 } = prepareInterval(interval1);
-  const { start: start2, end: end2 } = prepareInterval(interval2);
+  const { start: start1, end: end1 } = interval1;
+  const { start: start2, end: end2 } = interval2;
   if (start1 > end2 || start2 > end1) return null;
-  return unprepareInterval({
+  return {
     start: Math.max(start1, start2),
     end: Math.min(end1, end2)
-  });
+  };
 };
 
 export const isInInterval = (interval: Interval, value: Timestamp): boolean => {
-  if (!Number.isFinite(value)) return false;
-  const { start, end } = prepareInterval(interval);
+  if (!interval || !Number.isFinite(value)) return false;
+  const { start, end } = interval;
   return value >= start && value <= end;
 };
