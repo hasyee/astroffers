bba16cd86a38c417fe5a16e6e5caba11b740d861
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-12-06 00:09:37 +0100

    wip sun and moon extremes

--- src/calcs/moon.ts
+++ src/calcs/moon.ts
@@ -5,21 +5,29 @@ import { toNoon, toNextDay } from './time';
 import { getIntersection, isInInterval } from './interval';
 
 const getMoonRiseAndSet = (time: Timestamp, { lat, lon }: Loc): { rise: Timestamp; set: Timestamp } => {
-  const { rise: riseDate, set: setDate } = SunCalc.getMoonTimes(new Date(time), radToDeg(lat), radToDeg(lon));
+  const { rise: riseDate, set: setDate, alwaysUp, alwaysDown } = SunCalc.getMoonTimes(
+    new Date(time),
+    radToDeg(lat),
+    radToDeg(lon)
+  );
+  console.log(new Date(time), radToDeg(lat), radToDeg(lon), 'alwaysUp', alwaysUp, 'alwaysDown', alwaysDown);
+  if (alwaysUp) return { rise: -Infinity, set: Infinity };
+  if (alwaysDown) return { rise: undefined, set: undefined };
   const rise = riseDate ? riseDate.getTime() : undefined;
   const set = setDate ? setDate.getTime() : undefined;
   return { rise, set };
 };
 
-export const getMoonNight = (night: Interval, loc: Loc): Interval => {
+export const getMoonNight = (date: Timestamp, night: Interval, loc: Loc): Interval => {
   if (!night) return null;
-  const { rise: rise1, set: set1 } = getMoonRiseAndSet(night.start, loc);
-  const { rise: rise2, set: set2 } = getMoonRiseAndSet(night.end, loc);
+  const { rise: rise1, set: set1 } = getMoonRiseAndSet(toNoon(date), loc);
+  const { rise: rise2, set: set2 } = getMoonRiseAndSet(toNoon(toNextDay(date)), loc);
   if (isInInterval(night, rise1)) return { start: set1, end: rise1 };
   if (isInInterval(night, set1)) return { start: set1, end: rise2 };
   if (isInInterval(night, rise2)) return { start: set1, end: rise2 };
   if (isInInterval(night, set2)) return { start: set2, end: rise2 };
   if (set1 > rise1 && set1 < night.start && rise2 < set2 && rise2 > night.end) return { start: set1, end: rise2 };
+  if (set1 < rise1) console.log(rise1, set1, rise2, set2);
   return null;
 };
 
