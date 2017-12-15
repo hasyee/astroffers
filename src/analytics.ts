301acadc876cdf20f62b6cc3ed7a0e17c26ac561
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-12-15 09:00:29 +0100

    remove unused deps

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,6 +1,3 @@
-import { stringify } from 'qs';
-import { remote } from 'electron';
-import { machineIdSync } from 'node-machine-id';
 import Analytics from 'electron-ga';
 
 export default new Analytics('UA-110942439-1');
